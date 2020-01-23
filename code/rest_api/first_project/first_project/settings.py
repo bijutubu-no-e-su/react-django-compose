db9f9118a54330ab3a696d004c43cc683e5ff464
kaoru <kaoru@birman-mac.local>
2020-01-24 01:21:16 +0900

    add log directory and change pagination setting

--- code/rest_api/first_project/first_project/settings.py
+++ code/rest_api/first_project/first_project/settings.py
@@ -184,7 +184,7 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     # Pagination
-    'DEFAULT_PAGINATION_CLASS': 'common.drf.pagination.CommonPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     # Filters
     # 'DEFAULT_FILTER_BACKENDS': (
